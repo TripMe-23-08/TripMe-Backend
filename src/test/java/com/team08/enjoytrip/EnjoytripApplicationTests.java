0c3c9871cf912d10324591e13f9647a739a783bb
dms011221 <dms011221@gmail.com>
2023-04-27 09:58:17 +0900

    first commit

---
+++ src/test/java/com/team08/enjoytrip/EnjoytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team08.enjoytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoytripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
