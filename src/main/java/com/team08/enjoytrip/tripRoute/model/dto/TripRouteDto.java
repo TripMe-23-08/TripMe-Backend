82e0bc88e677731ef731e392e4967551ea918afd
Eundms <dms011221@gmail.com>
2023-05-21 13:17:46 +0900

    fix: tripRoute

--- src/main/java/com/team08/enjoytrip/tripRoute/model/dto/TripRouteDto.java
+++ src/main/java/com/team08/enjoytrip/tripRoute/model/dto/TripRouteDto.java
@@ -2,7 +2,16 @@ package com.team08.enjoytrip.tripRoute.model.dto;
 
 import java.sql.Timestamp;
 import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@ToString
+@Setter
+@NoArgsConstructor
 public class TripRouteDto {
     private int id;
     private String name;
@@ -12,76 +21,14 @@ public class TripRouteDto {
     private List<TripPlaceDto> tripPlaces;
 
 
-
-    public TripRouteDto() {
-    }
-
-    public TripRouteDto(int id, String name, Timestamp createdAt, int userId, String tripImgUrl, List<TripPlaceDto> tripPlaces) {
+   @Builder
+    public TripRouteDto(int id, String name, Timestamp createdAt, int userId, String tripImgUrl,
+                        List<TripPlaceDto> tripPlaces) {
         this.id = id;
         this.name = name;
         this.createdAt = createdAt;
         this.userId = userId;
         this.tripImgUrl = tripImgUrl;
         this.tripPlaces = tripPlaces;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getTripImgUrl() {
-        return tripImgUrl;
-    }
-
-    public List<TripPlaceDto> getTripPlaces() {
-        return tripPlaces;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public void setTripImgUrl(String tripImgUrl) {
-        this.tripImgUrl = tripImgUrl;
-    }
-
-    public void setTripPlaces(List<TripPlaceDto> tripPlaces) {
-        this.tripPlaces = tripPlaces;
-    }
-
-    @Override
-    public String toString() {
-        return "TripRouteDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", createdAt=" + createdAt +
-                ", userId=" + userId +
-                ", tripImgUrl='" + tripImgUrl + '\'' +
-                ", tripPlaces=" + tripPlaces +
-                '}';
     }
 }
